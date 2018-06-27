83f39a42847c98559339fb60dde4bec4f2faa07f
xjx <644233452@qq.com>
2018-06-27 18:00:08 +0800

    add cors

--- server/server.js
+++ server/server.js
@@ -11,14 +11,14 @@ const HOST = '0.0.0.0';
 const PORT = 3333;
 
 app.use(cors({
-  origin: function(ctx) {
-    return '*';
-  },
-  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
-  maxAge: 3000,
-  credentials: true,
-  allowMethods: ['GET', 'POST', 'DELETE'],
-  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    origin: function (ctx) {
+        return '*';
+    },
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+    maxAge: 3000,
+    credentials: true,
+    allowMethods: ['GET', 'POST', 'DELETE'],
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
 }));
 
 let server = app.listen(PORT, HOST, () => {
