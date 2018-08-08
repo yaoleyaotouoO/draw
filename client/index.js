7b05c87f27532d61ab5627bd6c870106ff503654
xjx <644233452@qq.com>
2018-08-08 16:39:26 +0800

    update app.vue to App.vue

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import App from './app.vue';
+import App from './App.vue';
 import ElementUI from 'element-ui';
 import VueX from 'vuex';
 import VueRouter from 'vue-router';
